41761751e3b9f6f90c4a4ad486fd9c025e07ce2d
ajmalmajeed97 <49806241+ajmalmajeed97@users.noreply.github.com>
2020-04-26 01:29:09 +0530

    add high and low forecast will do

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ callback('unable to connect service',undefined)
     callback('unable to fins location',undefined)
   } 
   else{
-callback(undefined, body.current.weather_descriptions[0] + "it is currently " + body.current.temperature + "degress out,there is a " + body.current.feelslike +  '%CHANCE OF RAIN')
+    
+callback(undefined, body.current.weather_descriptions[0] + " it is currently " + body.current.temperature + "degress out,there is a " + body.current.feelslike + "degres out,humidity is" + body.current.humidity )
   }
 })
 }
